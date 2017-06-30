c289b7979c0be72b02aebe0c6984effca3d0df6b
Scott Boone <sboone@trammell.ventures>
2017-06-30 18:56:01 -0500

    more basic framework, working through player entry use cases

--- forms.py
+++ forms.py
@@ -14,6 +14,8 @@ from wtforms import ValidationError
 
 from models import Player
 from models import Game
+from models import Entry
+from models import Collar
 
 #### helpers
 
@@ -27,10 +29,54 @@ def vld_new_user(form, field):
 #### custom forms
 
 class NewEntryForm(FlaskForm):
-    pass
+    playername = StringField('Player Name',
+            validators=[validators.InputRequired()]
+    )
+    wristband = StringField('Wristband #',
+            validators=[validators.InputRequired()]
+    )
+    collarid = StringField('Collar ID',
+            validators=[validators.InputRequired()]
+    )
+
+    def validate(self):
+        valid = FlaskForm.validate(self)
+        if not valid:
+            return False
+
+        if Player.player_exists(self.playername.data):
+            self.errors['general'] = 'player name already exists'
+            return False
+
+        if Player.wristband_exists(self.wristband.data):
+            self.errors['general'] = 'wristband already exists'
+            return False
+
+        collar = Collar.query.filter(Collar.collar_id == self.collarid.data.strip()).first()
+        #### TODO: change this if we want collar ids created on the fly
+        if not collar:
+            self.errors['general'] = 'invalid collar id'
+            return False
+        else:
+            if collar.entry.active:
+                self.errors['general'] = 'collar is already in use'
+                return False
+
+        return True
+
 
 class ReturnEntryForm(FlaskForm):
-    pass
+    wristband = StringField('Wristband #',
+            validators=[validators.InputRequired()]
+    )
+    collarid = StringField('Collar ID',
+            validators=[validators.InputRequired()]
+    )
+
+    ## VALIDATION
+    # wristband must exist in database
+    # collarid must be unused
+
 
 class ExitForm(FlaskForm):
     pass
--- models.py
+++ models.py
@@ -19,13 +19,10 @@ class Player(Base):
     wristband = db.Column(db.String())
     total_time_played = db.Column(db.DateTime())
     num_exits = db.Column(db.Integer())
-    visit_times = db.Column(JSON)
-    competing = db.Column(db.Boolean)
 
-    def __init__(self, name, wristband, collar):
+    def __init__(self, name, wristband):
         self.name = name 
         self.wristband = wristband
-        self.collar = collar
         self.total_time_played = 0
         self.last_entry_time = db.func.now()
         self.competing = True
@@ -40,16 +37,37 @@ class Player(Base):
             return True
         return False
 
+    @classmethod
+    def wristband_exists(cls, wristband):
+        p = Player.query.filter(Player.wristband == wristband).first()
+        if p:
+            return True
+        return False
+
+
 class Entry(Base):
-    """ Data related to a current player's active entry in the competition. """
+    """ Data related to a player's entry in the competition. """
     __tablename__ = 'entry'
 
     player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-    player = db.relationship('Player', backref=db.backref('entrys', lazy='dynamic')) 
-    collar = db.Column(db.String())
-    entrance_time = db.Column(db.DateTime())
+    player = db.relationship('Player', backref=db.backref('entry', lazy='dynamic')) 
+    exit_time = db.Column(db.DateTime())
     grace_until = db.Column(db.DateTime())
     num_plays = db.Column(db.Integer)
+    active = db.Column(db.Boolean)
+
+    def __init__(self, player):
+        self.player_id = player.id
+        self.player = player
+        self.grace_until = db.func.now()
+
+
+class Collar(Base):
+    """ """
+    __tablename__ = 'collar'
+    collar_id = db.Column(db.String)
+    entry_id = db.Column(db.Integer, db.ForeignKey('entry.id'))
+    entry = db.relationship('Entry', backref=db.backref('collar', lazy='dynamic'))
 
 class Game(Base):
     """ A game in the system. """
@@ -58,6 +76,4 @@ class Game(Base):
     name = db.Column(db.String())
     num_players = db.Column(db.Integer())
     active = db.Column(db.Boolean)
-    
-
 
